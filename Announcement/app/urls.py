f9985dbd9a67eeefc2f80760d8831e8a3cb98c54
Egor <05murik25@mail.ru>
2022-06-23 11:00:57 +0300

    post

--- Announcement/app/urls.py
+++ Announcement/app/urls.py
@@ -1,1 +1,11 @@
 from django.urls import path
+from .views import RegistrationAPIView
+
+
+app_name = 'app'
+
+
+urlpatterns = [
+    path('users/', RegistrationAPIView.as_view()),
+    path('users/<int:pk>', RegistrationAPIView.as_view()),
+]
