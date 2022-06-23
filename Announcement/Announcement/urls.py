36dfa1b22d423f3e79a7756e36d7bcf3dcc1e80d
Egor <05murik25@mail.ru>
2022-06-23 10:30:13 +0300

    models

--- Announcement/Announcement/urls.py
+++ Announcement/Announcement/urls.py
@@ -18,7 +18,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('app.urls', namespace='app')),
+    # path('api/', include('app.urls', namespace='app')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- Announcement/app/models.py
+++ Announcement/app/models.py
@@ -1,3 +1,140 @@
+import jwt
+
+from datetime import datetime, timedelta
+
+from django.conf import settings
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
+
 from django.db import models
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    """
+    Django требует, чтобы кастомные пользователи определяли свой собственный
+    класс Manager. Унаследовавшись от BaseUserManager, мы получаем много того
+    же самого кода, который Django использовал для создания User (для демонстрации).
+    """
+
+    def create_user(self, name, email, phone, login, tg, birth, password=None):
+        """ Создает и возвращает пользователя с имэйлом, паролем и именем. """
+        if name is None:
+            raise TypeError('Users must have a username.')
+
+        if phone is None:
+            raise TypeError('Users must have a phone number.')
+
+        if login is None:
+            raise TypeError('Users must have a login.')
+
+        if birth is None:
+            raise TypeError('Users must have a birth.')
+
+        user = self.model(login=login, name=name, phone=phone,
+                          birth=birth, tg=tg, email=self.normalize_email(email),)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, login, password):
+        """ Создает и возввращет пользователя с привилегиями суперадмина. """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.model(login=login)
+        user.set_password(password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    # Каждому пользователю нужен понятный человеку уникальный идентификатор,
+    # который мы можем использовать для предоставления User в пользовательском
+    # интерфейсе. Мы так же проиндексируем этот столбец в базе данных для
+    # повышения скорости поиска в дальнейшем.
+    name = models.CharField(db_index=True, max_length=255, unique=True, null=True)
+
+    # Так же мы нуждаемся в поле, с помощью которого будем иметь возможность
+    # связаться с пользователем и идентифицировать его при входе в систему.
+    # Поскольку адрес почты нам нужен в любом случае, мы также будем
+    # использовать его для входы в систему, так как это наиболее
+    # распространенная форма учетных данных на данный момент (ну еще телефон).
+    email = models.EmailField(unique=True, blank=True, null=True)
+    phone = models.CharField(max_length=12, null=True)
+    login = models.CharField(max_length=16, db_index=True, unique=True, null=True)
+    tg = models.CharField(max_length=16, blank=True, null=True)
+    birth = models.DateField(db_index=True, null=True)
+
+
+    # Когда пользователь более не желает пользоваться нашей системой, он может
+    # захотеть удалить свой аккаунт. Для нас это проблема, так как собираемые
+    # нами данные очень ценны, и мы не хотим их удалять :) Мы просто предложим
+    # пользователям способ деактивировать учетку вместо ее полного удаления.
+    # Таким образом, они не будут отображаться на сайте, но мы все еще сможем
+    # далее анализировать информацию.
+    is_active = models.BooleanField(default=True)
+
+    # Этот флаг определяет, кто может войти в административную часть нашего
+    # сайта. Для большинства пользователей это флаг будет ложным.
+    is_staff = models.BooleanField(default=False)
+
+    # Временная метка создания объекта.
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    # Временная метка показывающая время последнего обновления объекта.
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Дополнительный поля, необходимые Django
+    # при указании кастомной модели пользователя.
+
+    # Свойство USERNAME_FIELD сообщает нам, какое поле мы будем использовать
+    # для входа в систему. В данном случае мы хотим использовать почту.
+    USERNAME_FIELD = 'login'
+    REQUIRED_FIELDS = []
+
+    # Сообщает Django, что определенный выше класс UserManager
+    # должен управлять объектами этого типа.
+    objects = UserManager()
+
+    def __str__(self):
+        """ Строковое представление модели (отображается в консоли) """
+        return self.login
+
+    @property
+    def token(self):
+        """
+        Позволяет получить токен пользователя путем вызова user.token, вместо
+        user._generate_jwt_token(). Декоратор @property выше делает это
+        возможным. token называется "динамическим свойством".
+        """
+        return self._generate_jwt_token()
+
+    def get_full_name(self):
+        """
+        Этот метод требуется Django для таких вещей, как обработка электронной
+        почты. Обычно это имя фамилия пользователя, но поскольку мы не
+        используем их, будем возвращать username.
+        """
+        return self.name
+
+    def get_short_name(self):
+        """ Аналогично методу get_full_name(). """
+        return self.name
+
+    def get_birth(self):
+        return self.birth
+
+    def _generate_jwt_token(self):
+        """
+        Генерирует веб-токен JSON, в котором хранится идентификатор этого
+        пользователя, срок действия токена составляет 1 день от создания
+        """
+        dt = datetime.now() + timedelta(days=1)
+        token = jwt.encode({
+            'id': self.pk,
+            'exp': int(dt.strftime('%s'))
+        }, settings.SECRET_KEY, algorithm='HS256')
+        return token
